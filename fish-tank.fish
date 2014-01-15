e2a02ccac4828df193ac689b83802b787960cd23
Will Farrell <will@mojotech.com>
2014-01-14 23:13:41 -0500

    wip

---
+++ fish-tank.fish
@@ -0,0 +1,14 @@
+
+. $fish_tank_dir/*.fish
+
+tank plugin 'bpinto/oh-my-fish' 'plugins/git'
+tank plugin 'bpinto/oh-my-fish' 'plugins/brew'
+tank plugin 'bpinto/oh-my-fish' 'plugins/node'
+tank plugin 'bpinto/oh-my-fish' 'plugins/vi-mode'
+
+tank plugin 'JeanMertz/chruby-fish' 'share/chruby/chruby.fish'
+tank plugin 'JeanMertz/chruby-fish' 'share/chruby/auto.fish'
+tank plugin 'wkf/chnode'
+tank plugin 'wkf/chnode' 'chnode-auto'
+tank plugin 'wkf/node-install'
+
