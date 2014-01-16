72aaa3be618c7a986cae2b8e6a3d520f35f32680
Will Farrell <will@mojotech.com>
2014-01-15 20:34:58 -0500

    wip

--- fish-tank.fish
+++ fish-tank.fish
@@ -1,14 +1,50 @@
 
 . $fish_tank_dir/*.fish
 
-tank plugin 'bpinto/oh-my-fish' 'plugins/git'
-tank plugin 'bpinto/oh-my-fish' 'plugins/brew'
-tank plugin 'bpinto/oh-my-fish' 'plugins/node'
-tank plugin 'bpinto/oh-my-fish' 'plugins/vi-mode'
+#tank plugin 'bpinto/oh-my-fish' 'plugins/git'
+#tank plugin 'bpinto/oh-my-fish' 'plugins/brew'
+#tank plugin 'bpinto/oh-my-fish' 'plugins/node'
+#tank plugin 'bpinto/oh-my-fish' 'plugins/vi-mode'
 
-tank plugin 'JeanMertz/chruby-fish' 'share/chruby/chruby.fish'
-tank plugin 'JeanMertz/chruby-fish' 'share/chruby/auto.fish'
-tank plugin 'wkf/chnode'
-tank plugin 'wkf/chnode' 'chnode-auto'
-tank plugin 'wkf/node-install'
+#tank plugin 'JeanMertz/chruby-fish' 'share/chruby/chruby.fish'
+#tank plugin 'JeanMertz/chruby-fish' 'share/chruby/auto.fish'
+#tank plugin 'wkf/chnode'
+#tank plugin 'wkf/chnode' 'chnode-auto'
+#tank plugin 'wkf/node-install'
+
+tank 'git'
+tank 'brew'
+tank 'node'
+
+tank 'chruby'
+tank 'chruby/auto'
+tank 'chnode'
+tank 'chnode/auto'
+
+tank 'vi-mode'
+
+
+
+repository: bpinto/oh-my-fish
+sha: e2a02ccac4828df193ac689b83802b787960cd23
+path: plugins/git
+load:
+  -
+  -
+  -
+  -
+
+
+[repository]
+bpinto/oh-my-fish
+
+[sha]
+
+[path]
+
+[load]
+
+[functions]
+
+
 
